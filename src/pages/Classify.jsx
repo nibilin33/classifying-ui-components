242848896ce9d4251c75297cd61b678da7127757
nibilin33 <nibilin33@gmail.com>
2023-06-01 10:01:57 +0800

    feat: modify entry structural

---
+++ src/pages/Classify.jsx
@@ -0,0 +1,4 @@
+// const classifier = ml5.imageClassifier('path/to/custom/model.json', modelReady);
+export default function Classify() {
+    return <div>classify</div>
+}
---
+++ src/pages/Trainning.jsx
@@ -0,0 +1,80 @@
+import { useState, useEffect, useRef } from "react";
+import * as ml5 from "ml5";
+//[featureExtractor] featureExtractor for images from file https://github.com/ml5js/ml5-library/issues/887
+// 一个分类至少要两张照片
+export default function Trainning() {
+  const version = ml5.version;
+  const inputRef = useRef(null);
+  const [classifier, setClassifier] = useState(null);
+  const [result, setResult] = useState("--");
+  const [training, setTraining] = useState(false);
+  let currentLabel = "";
+  const setup = () => {
+    const featureExtractor = ml5.featureExtractor("MobileNet", () => {
+      console.log("Model Ready");
+    });
+    setClassifier(featureExtractor.classification());
+  };
+  useEffect(() => {
+    setup();
+  }, []);
+  function gotResult(e) {
+    setResult('--');
+    const { target } = e;
+    target.files.forEach((file) => {
+      let img = new Image();
+      img.src = window.URL.createObjectURL(file);
+      img.onload = () => {
+        classifier.classify(img, (err, result) => {
+          console.log("result", result);
+          if (err) return;
+          setResult(result[0].label);
+        });
+      };
+    });
+    inputRef.current.value =  null;
+  }
+
+  function train() {
+    setTraining(true);
+    classifier.train((lossValue) => {
+      console.log("Loss is", lossValue);
+      if (!lossValue) {
+        setTraining(false);
+      }
+    });
+  }
+  function save() {
+    classifier.save();
+  }
+  function hanldeInput(e) {
+    const { target } = e;
+    currentLabel = target.value;
+  }
+  const handleImage = (e) => {
+    const { target } = e;
+    target.files.forEach((file) => {
+      let img = new Image();
+      img.src = window.URL.createObjectURL(file);
+      img.onload = () => {
+        classifier.addImage(img, currentLabel, () => {
+          console.log("image added");
+        });
+      };
+    });
+  };
+  return (
+    <>
+        <h1>ML5 version:{version} </h1>
+        <label>当前文件打标签</label>
+        <input type="text" label="标签" onInput={hanldeInput}></input>
+        <input type="file" accept="image/*" onChange={handleImage}></input>
+        <button onClick={train}>训练</button>
+        {training? <p>训练中...</p> : <p></p>}
+        <label>测试识别结果</label>
+        <input ref={inputRef}  type="file" accept="image/*" onChange={gotResult}></input>
+        <p>识别结果:{result}</p>
+        <button onClick={save}>保存</button>
+    </>
+  );
+}
